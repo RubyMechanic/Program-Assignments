867d65d5664d27c7180dd9e2ab2fc000ac8c2da6
Ruby Mechanic <rubymechanic@Rubys-MacBook-Pro.local>
2021-06-16 09:48:37 -0700

    adding python to file

--- snoopdogg.py
+++ snoopdogg.py
@@ -5,4 +5,6 @@ salary = 0.68
 print(name+" is "+ height + " and has ")
 print(platinum)
 print("platinum records")
-print(name + " makes" + salary +" dollars per second")
+print(name + " makes")
+print(salary)
+print(" dollars per second")
