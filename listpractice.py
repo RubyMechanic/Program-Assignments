60f1217ff9fab675c92d20b0f1f7c0dba935205f
Ruby Mechanic <rubymechanic@Rubys-MacBook-Pro.local>
2021-06-17 12:00:42 -0700

    adding file to git

---
+++ listpractice.py
@@ -0,0 +1,18 @@
+empty_list = []
+cities = ["Oakland", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(cities)
+US_cities = cities[0:6]
+print(US_cities)
+cities[0] = "San Fransisco"
+cities[1] = "Brooklyn"
+cities[6] = "Hollywood"
+cities[5] = "Tampa"
+print(US_cities)
+print(cities)
+
+
+
+favorite_food = ["pizza", "chicken sandwich", "blueberries", "watermelon", "cherries", "coffee", "milkshake", "popcorn", "bagel", "ginger ale"]
+print(favorite_food)
+threefaves = favorite_food[0:3]
+print(threefaves)
