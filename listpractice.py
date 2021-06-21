917833c73a753611b8e2a0627a0ad6cd9f5e7226
Ruby Mechanic <rubymechanic@Rubys-MacBook-Pro.local>
2021-06-21 12:03:49 -0700

    upgrade

--- listpractice.py
+++ listpractice.py
@@ -1,3 +1,4 @@
+import numpy as np
 empty_list = []
 cities = ["Oakland", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
 print(cities)
@@ -9,10 +10,26 @@ cities[6] = "Hollywood"
 cities[5] = "Tampa"
 print(US_cities)
 print(cities)
-
-
+cities.sort()
+print(cities)
+cities.sort(key=len)
+print(cities)
 
 favorite_food = ["pizza", "chicken sandwich", "blueberries", "watermelon", "cherries", "coffee", "milkshake", "popcorn", "bagel", "ginger ale"]
 print(favorite_food)
 threefaves = favorite_food[0:3]
 print(threefaves)
+
+
+listnumbers = []
+size = 5
+for i in range(size):
+    listnumbers.append(10*np.random.random())
+print(listnumbers)
+listnumbers.sort() 
+print(listnumbers)      
+i=1
+
+#pop, remove, clear
+#append, extend, insert
+
